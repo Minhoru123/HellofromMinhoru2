3d71b022f3e55ad2c7897edc0567d3e3fa892860
ToddWyattNelson <nel17052@byui.edu>
2020-01-31 12:31:03 -0700

    hello wyatt

--- src/cotache/main.java
+++ src/cotache/main.java
@@ -3,5 +3,7 @@ package cotache;
 public class main {
     public static void main(String[] args) {
         System.out.println("HellofromMinhoru");
+        System.out.println("Hello From Wyatt");
+        
     }
 }
