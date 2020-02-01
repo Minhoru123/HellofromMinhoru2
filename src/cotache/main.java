fb0240e8304b83428e015eb08c92b6153fa6c5e1
Minhoru <mijay_1998@hotmail.com>
2020-02-01 16:51:46 -0700

    Changed name

--- src/cotache/main.java
+++ src/cotache/main.java
@@ -2,6 +2,6 @@ package cotache;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("HellofromMinhoru");
+        System.out.println("Hello my name is Minhoru.");
     }
 }
